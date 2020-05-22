ae0cee2df1d6ac89e9dab341209d0630dda9b9f4
Traveler Assistance Network <60201114+TAS-Network@users.noreply.github.com>
2020-05-21 19:35:17 -0500

    Odoo Modulo Lista Canal
    Odoo Modulo Lista Canal

---
+++ listas/models/lista.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class lista(models.Model):
+    _name = 'lista.canal'
+    name = fields.Char(string='descripcion')
+    
+class lista(models.Model):
+    _inherit = 'crm.lead'
+    lista_id = fields.Many2one('lista.canal', string='Canales')
