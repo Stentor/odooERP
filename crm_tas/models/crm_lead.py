fbc7b283b3b4261d1046773b5bf7f3b2658a1c5c
TAS NETWORK <desarrollo@pervolare.com>
2020-05-26 09:32:54 -0500

    [26-05-2020] Revision de Errores en MOdelos

--- crm_tas/models/crm_lead.py
+++ crm_tas/models/crm_lead.py
@@ -7,42 +7,22 @@ class crmLeadChannel(models.Model):
     _description = "Canales"
     name = fields.Char(string='Channel')
 
-# Create Relationship Model
-class crmLeadChannelRelationship(models.Model):
-    _inherit = 'crm.lead'
-    channel_id = fields.Many2one('crm.lead.channel', string='Channel')
-
 # Create Model Plan
 class crmLeadPlan(models.Model):
     _name = 'crm.lead.plan'
     _description = "Planes"
     name = fields.Char(string='Plan')
-    
-class crmLeadPLanRelationship(models.Model):
-    _inherit = 'crm.lead'
-    plan_id = fields.Many2one('crm.lead.plan', string='Plan')
 
 # Create Model Medium
 class crmLeadMedium(models.Model):
     _name = 'crm.lead.medium'
     _description = "Medios"
     name = fields.Char(string='Medium')
-    
-class crmLeadMediumRelationship(models.Model):
-    _inherit = 'crm.lead'
-    medium_id = fields.Many2one('crm.lead.medium', string='Medium')
-
 # Create model Payment
 class crmLeadPaymen(models.Model):
     _name = 'crm.lead.payment'
     _description = "Formas de Pago"
     name = fields.Char(string='Payment')
-
-# Create Relationship Model    
-class crmLeadPaymentRelationship(models.Model):
-    _inherit = 'crm.lead'
-    payment_id = fields.Many2one('crm.lead.payment', string='Payment')
-
 
 # Create model fraction payment
 class crmLeadPaymenFraction(models.Model):
@@ -50,7 +30,18 @@ class crmLeadPaymenFraction(models.Model):
     _description = "Formas de Pago Fracción"
     name = fields.Char(string='Payment Fraction')
 
-# Create Relationship Model    
-class crmLeadPaymentFractionRelationship(models.Model):
+# Create Relationship Model
+class crmLead(models.Model):
     _inherit = 'crm.lead'
+    channel_id = fields.Many2one('crm.lead.channel', string='Channel')
+    plan_id = fields.Many2one('crm.lead.plan', string='Plan')
+    medium_id = fields.Many2one('crm.lead.medium', string='Medium')
+    payment_id = fields.Many2one('crm.lead.payment', string='Payment')
     payment_id = fields.Many2many('crm.lead.payment.fraction', string='Payment Fraction')
+    
+
+
+
+    
+
+    
