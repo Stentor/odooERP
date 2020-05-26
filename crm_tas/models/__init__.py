41fe55dad1ab0897811b9242e8887a08c4906678
TAS NETWORK <desarrollo@pervolare.com>
2020-05-26 13:57:30 -0500

    [26-05-2020] Comentado helpdesk para revision

--- crm_tas/models/__init__.py
+++ crm_tas/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import crm_lead
 from . import res_partner
-from . import helpdesk_ticket
+#from . import helpdesk_ticket
--- crm_tas/models/res_partner.py
+++ crm_tas/models/res_partner.py
@@ -3,9 +3,7 @@ from odoo import api, fields, models, _
 
 # Codigos de Descuento
 class ResPartnerCode(models.Model):
-    #nombre del objeto
     _name = 'res.partner.code'
-    #nombre a visualizar
     _description = "Códigos de Descuento"
     name = fields.Char(string='Code')
     
