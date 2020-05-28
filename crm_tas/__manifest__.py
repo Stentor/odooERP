36531b28839d147da8c8b92e12a63d5cb91ec1f3
TAS NETWORK <desarrollo@pervolare.com>
2020-05-28 15:25:54 -0500

    Creacion vista en Helpdesk

--- crm_tas/__manifest__.py
+++ crm_tas/__manifest__.py
@@ -19,6 +19,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/crm_lead_view.xml',
+        'views/helpdesk_ticket_view.xml',
     ],
     'images': ['static/description/icon.png'],
     # data files containing optionally loaded demonstration data
