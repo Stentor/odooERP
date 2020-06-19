0f565da9b8b5d225587d92265d50f8fbc7d2159a
TAS NETWORK <desarrollo@pervolare.com>
2020-06-19 16:17:24 -0500

    Actualizando version para paso a produccion

--- crm_tas/__manifest__.py
+++ crm_tas/__manifest__.py
@@ -4,7 +4,7 @@
     'name': "CRM TAS",
     'summary': """
         Personalización con nuevos campos relacionados en Contactos, CRM, HELPDESK""",
-    'version': '0.2',
+    'version': '0.3',
     'depends': [
         'base',
         'crm',
