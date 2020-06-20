d75f192a93bbfbd6ddc9e716716a9ded1c026d96
TAS NETWORK <desarrollo@pervolare.com>
2020-06-20 16:54:01 -0500

    Correccion calculo de descuento %

--- crm_tas/__manifest__.py
+++ crm_tas/__manifest__.py
@@ -4,7 +4,7 @@
     'name': "CRM TAS",
     'summary': """
         Personalización con nuevos campos relacionados en Contactos, CRM, HELPDESK""",
-    'version': '0.6',
+    'version': '0.7',
     'depends': [
         'base',
         'crm',
